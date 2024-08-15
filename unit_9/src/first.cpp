8ad6cc7baafabe002ec5934a381c40d3aa7ad6ff
tigood <3020422083@qq.com>
2024-08-15 12:35:42 +0800

    访问元素小节完成

--- unit_9/src/first.cpp
+++ unit_9/src/first.cpp
@@ -14,9 +14,23 @@ void text_1()
     std::cout << "尾元素：" << ivect.back() << std::endl;
 }
 
+void text_2()
+{
+    std::vector<int> ivect = { 1, 2, 3 };
+    auto value1 = ivect.at(0);
+    auto value2 = ivect[0];
+    auto value3 = *ivect.begin();
+    auto value4 = ivect.front();
+    std::cout << "value1 = " << value1 << std::endl;
+    std::cout << "value2 = " << value2 << std::endl;
+    std::cout << "value3 = " << value3 << std::endl;
+    std::cout << "value4 = " << value4 << std::endl;
+}
+
 int main()
 {
-    text_1();
+    // text_1();
+    text_2();
 
     return 0;
 }
